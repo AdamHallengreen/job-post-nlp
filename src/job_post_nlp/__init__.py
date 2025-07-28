0817b5d663e7cfd2d4ff72242a0d9072ccb3e4c7
AskerNC <hms467@econ.ku.dk>
2025-07-28 17:35:34 +0200

    trying again

--- src/job_post_nlp/__init__.py
+++ src/job_post_nlp/__init__.py
@@ -1,4 +1,3 @@
-# mypy: disable-error-code=all
 # This is just a hack to make uv understand that the dependency is not redundant
-import da_core_news_lg  # noqa: F401
-import da_core_news_sm  # noqa: F401
+import da_core_news_lg  # type: ignore  # noqa: F401, PGH003
+import da_core_news_sm  # type: ignore  # noqa: F401, PGH003
