9f737fa0093acfa5603163acff94903b180074de
AskerNC <hms467@econ.ku.dk>
2025-07-28 17:29:20 +0200

    omg!

--- src/job_post_nlp/__init__.py
+++ src/job_post_nlp/__init__.py
@@ -1,4 +1,4 @@
-# type: ignore[import-untyped]
+# mypy: disable-error-code=all
 # This is just a hack to make uv understand that the dependency is not redundant
 import da_core_news_lg  # noqa: F401
 import da_core_news_sm  # noqa: F401
